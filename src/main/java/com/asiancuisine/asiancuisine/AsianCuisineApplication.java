673958eb3573aea6487cb5cb11307c433d9ca848
gitzhikang <784280896@qq.com>
2024-10-22 19:35:20 -0400

    init

---
+++ src/main/java/com/asiancuisine/asiancuisine/AsianCuisineApplication.java
@@ -0,0 +1,13 @@
+package com.asiancuisine.asiancuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsianCuisineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsianCuisineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asiancuisine/asiancuisine/AsianCuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiancuisine.asiancuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsianCuisineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

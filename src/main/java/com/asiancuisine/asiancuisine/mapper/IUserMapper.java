b94c9eea9867c48a0e2a84f371c32bf470a1c901
gitzhikang <784280896@qq.com>
2024-10-23 22:43:03 -0400

    database

--- src/main/java/com/asiancuisine/asiancuisine/mapper/IUserMapper.java
+++ src/main/java/com/asiancuisine/asiancuisine/mapper/IUserMapper.java
@@ -1,6 +1,7 @@
 package com.asiancuisine.asiancuisine.mapper;
 
 import com.asiancuisine.asiancuisine.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper

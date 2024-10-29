33eaffcaf0e6b406a1c5af68859d09cdb0cfe000
gitzhikang <784280896@qq.com>
2024-10-29 15:42:45 -0400

    upload image

--- src/main/java/com/asiancuisine/asiancuisine/controller/CommonController.java
+++ src/main/java/com/asiancuisine/asiancuisine/controller/CommonController.java
@@ -3,6 +3,7 @@ package com.asiancuisine.asiancuisine.controller;
 import com.asiancuisine.asiancuisine.Result.Result;
 import com.asiancuisine.asiancuisine.util.AwsS3Util;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,6 +30,7 @@ public class CommonController {
     @Value("${ac.aws.s3.bucketName}")
     private String bucketName;
 
+    @ApiOperation("Upload file to Aws s3")
     @PostMapping("/upload")
     public Result<List<String>> upload(@RequestParam("files") MultipartFile[] files){
         List<String> uploadedUrls = new ArrayList<>();
@@ -39,8 +41,8 @@ public class CommonController {
             return Result.success(uploadedUrls);
 
         } catch (IOException e) {
-            log.error("文件上传失败:{}",e);
+            log.error("File Upload Failed:{}",e);
         }
-        return Result.error("文件上传失败");
+        return Result.error("File Upload Failed");
     }
 }

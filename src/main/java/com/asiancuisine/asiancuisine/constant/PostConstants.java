b66d0fd48406ab41579e840f703032a10881712b
gitzhikang <784280896@qq.com>
2024-12-03 16:33:44 -0500

    For redeploy backend server through CICD

--- src/main/java/com/asiancuisine/asiancuisine/constant/PostConstants.java
+++ src/main/java/com/asiancuisine/asiancuisine/constant/PostConstants.java
@@ -3,5 +3,5 @@ package com.asiancuisine.asiancuisine.constant;
 import java.util.concurrent.TimeUnit;
 
 public class PostConstants {
-   public static final Long CACHE_SCROLL_ID_EXPIRE_TIME = TimeUnit.MINUTES.toMillis(2);;
+   public static final Long CACHE_SCROLL_ID_EXPIRE_TIME = TimeUnit.MINUTES.toMillis(10);;
 }
